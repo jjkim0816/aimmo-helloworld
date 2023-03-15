f7efeba8626fd3fa2c5e2fe5a9a67e0a8c98ab59
Austin(BE) <jjkim0816@gmail.com>
2023-03-15 14:01:46 +0900

    fix:modify response when request api

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world() -> str:  # put application's code here
-    return "Hello World!!!-test"
+    return "Hello World!!!-test-123"
 
 
 if __name__ == "__main__":

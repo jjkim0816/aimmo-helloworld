d0f8147983eae629eff3b3a77380156c34313428
Austin(BE) <jjkim0816@gmail.com>
2023-03-16 10:09:06 +0900

    fix:modify 'Always' on imagePullPolicy, and modify response data for requesting api

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world() -> str:  # put application's code here
-    return "Hello World!!!-test-123"
+    return "Hello World!!!-test-123-always"
 
 
 if __name__ == "__main__":
